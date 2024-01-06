64fb42b5c99ee69483d2a19c1df2db0d11d0c140
tkudlinski <tomasz.kudlinski@gmail.com>
2024-01-06 17:11:21 +0100

    struct

--- struct/src/main.rs
+++ struct/src/main.rs
@@ -1,9 +1,10 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Lang {
   English,
   Spanish,
   Chinese,
   Texan,
+  Polish
 }
 
 struct Greeting {
@@ -22,8 +23,16 @@ fn main() {
   v.push(g);
   let g : Greeting = Greeting { lang: Lang::Chinese, message: String::from("WasmEdge 你好!") };
   v.push(g);
+  let g : Greeting = Greeting { lang: Lang::Polish, message: String::from("Witam WasmEdge!") };
+  v.push(g);
 
-  for e in v {
+  for e in &v {
     println!("{:?} {}", e.lang, e.message);
+  }
+
+  let maybe_greeting = v.into_iter().find(|item| item.lang == Lang::Polish);
+
+  if let Some(greeting) = maybe_greeting {
+    println!("Msg in polish: {}", greeting.message);
   }
 }

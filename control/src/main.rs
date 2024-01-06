7b67cea26964c337cb450cb5a5d7da97baf9d27a
tkudlinski <tomasz.kudlinski@gmail.com>
2024-01-06 16:51:13 +0100

    control flows

--- control/src/main.rs
+++ control/src/main.rs
@@ -1,15 +1,21 @@
+const WE: &str = "WasmEdge";
+
 fn main() {
-  for i in 0..10 {
-    match i {
-      0 => println!("Hello WasmEdge!"),
-      1 => println!("Howdy WasmEdge!"),
-      2 => println!("Howdy WasmEdge!"),
-      3 => println!("Hola WasmEdge!"),
-      4 => println!("Bonjour WasmEdge!"),
-      5 => println!("guten tag WasmEdge!"),
-      6 => println!("WasmEdge 你好!"),
-      7 => println!("こんにちは  WasmEdge!"),
-      _ => println!("Salve WasmEdge!"),
+    for i in 0..10 {
+        if i < 3 {
+            println!("Howdy {}!", WE);
+        } else if i == 3 {
+            println!("Hola {}!", WE);
+        } else if i == 4 {
+            println!("Bonjour {}!", WE);
+        } else if i == 5 {
+            println!("guten tag {}!", WE);
+        } else if i == 6 {
+            println!("WasmEdge 你好!");
+        } else if i == 7 {
+            println!("こんにちは {}!", WE);
+        } else {
+            println!("Salve {}!", WE)
+        }
     }
-  }
 }
